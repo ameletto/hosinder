b1c3a888472bf15862c694da43db14b13ebee6f7
ameletto <meamymli@gmail.com>
2021-08-19 06:36:22 -0400

    Beautifying the home screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,33 +5,31 @@ export default function Home() {
     return (
         // <div className= font>
         <div>
-        <p className = "absolute left-5 truncate z-10 text-gray-50" style={{fontSize: 30}}> HOSA TINDER HOSA TINDER HOSA TINDER HOSA TINDER HOSA TINDER HOSA TINDER HOSA TINDER HOSA</p> 
-        <p className = "absolute right-0 top-0" style={{transform: "rotate(90deg)"}}> TINDER HOSA TINDER HOSA TINDER HOSA TINDER HOSA TINDER </p>
-        <p className = "absolute right-0 bottom-0" style={{transform: "rotate(180deg)"}}> HOSA TINDER HOSA TINDER HOSA TINDER HOSA TINDER HOSA TINDER HOSA TINDER HOSA TINDER HOSA </p>
-        <p className = "absolute left-0 bottom-0" style={{transform: "rotate(270deg)"}}> TINDER HOSA TINDER HOSA TINDER HOSA TINDER HOSA TINDER</p>
-        
-        <div className = "absolute w-96 h-96 rounded-full z-0" style={{background: "radial-gradient(rgba(2,1,31,1) -10%, rgba(5,25,138,1) 26%, rgba(0,212,255,1) 95%)"}}>
-        </div>
-        
-        <div className="flex items-center justify-center flex-col h-screen michroma font-black">
-            <div className="flex flex-row">
-            {/* <HandwrittenButton onClick={() => console.log("button is pressed!")}>
-                Sign-up 
-                </HandwrittenButton> */}
-                <SignInButton/> 
+            <p className="absolute left-3 truncate z-10 oswald font-bold" style={{ fontSize: 36 }}> HOSA TINDER HOSA TINDER HOSA TINDER HOSA TINDER HOSA TINDER HOSA TINDER HOSA TINDER HOSA</p>
+            <p className="absolute top-right truncate z-10 oswald font-bold" style={{ transform: "rotate(90deg)", fontSize: 36 }}> TINDER HOSA TINDER HOSA TINDER HOSA TINDER HO </p>
+            <p className="absolute right-3 truncate bottom-0 z-10 oswald font-bold" style={{ transform: "rotate(180deg)", fontSize: 36 }}> SA TINDER HOSA TINDER HOSA TINDER HOSA TINDER HOSA TINDER HOSA TINDER HOSA TINDER HOSA TI</p>
+            <p className="absolute bottom-left truncate z-10 oswald font-bold" style={{ transform: "rotate(270deg)", fontSize: 36 }}> NDER HOSA TINDER HOSA TINDER HOSA TINDER HOSA</p>
+
+            <div className="absolute w-96 h-96 rounded-full z-0 left-overflow" style={{ background: "radial-gradient(rgba(183,225,252,1) 30%, rgba(212,208,254,0.5) 60%, rgba(255,255,255,0) 100%)" }}>
             </div>
-        <h1>Welcome to HOSA Tinder</h1>
-        <h2>Find your perfect event.</h2>
+            
+            <div className="absolute w-96 h-96 rounded-full right-0 bottom-0 z-0 right-overflow" style={{ background: "radial-gradient(rgba(183,225,252,1) 30%, rgba(212,208,254,0.5) 60%, rgba(255,255,255,0) 100%)" }}>
+            </div>
 
-        {/* textShadowColor: "black", textShadowOffset: {width: 10, height: 10}, textShadowRadius: 10 */}
-        </div>
-
-
-
+            <div className="flex items-center justify-center flex-col h-screen work-sans font-bold">
+                <p className="text-7xl z-10">Welcome to HOSA Tinder</p>
+                <p className="text-5xl z-10 p-3">Find your perfect event.</p>
+                <div className="p-2"></div>
+                <div className="flex flex-row rounded-full montserrat text-3xl p-5" style={{ color: "rgba(255,255,255,1)", background: "rgba(0,0,0,1)" }}>
+                    <SignInButton/>
+                </div>  
+            </div>
         </div>
     );
 }
 
-// class = className
-// save
-// div
+{/* <HandwrittenButton onClick={() => console.log("button is pressed!")}>
+                Sign-up 
+                </HandwrittenButton> */}
+
+{/* textShadowColor: "black", textShadowOffset: {width: 10, height: 10}, textShadowRadius: 10 */}
