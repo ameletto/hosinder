e82d9064e9ab76a3d7d931d96a0877f5d2d08b26
Laura Gao <lauragao86868@gmail.com>
2021-09-18 19:00:05 -0400

    basic ability to submit top3 events

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -10,14 +10,19 @@ import { DragDropContext, Droppable, Draggable, resetServerContext } from "react
 import EventCard from "../components/EventCard";
 import axios from "axios";
 import { useState } from "react";
+import HandwrittenButton from "../components/HandwrittenButton";
+import Modal from "../components/Modal";
+import Button from "../components/Button";
 
-
+// TODO: allow admin of school to see all submissions.
+// TODO: congratulatory page for submitting events, maybe go to social media page of all submissions? 
+// also maybe throw some confetti like vercel.
 const dashboard = (props: {thisUser: DatedObj<UserObj>, preferredEvents: DatedObj<EventObj>[]}) => {
     const [preferredEvents, setPreferredEvents] = useState<DatedObj<EventObj>[]>(props.preferredEvents);
-    console.log(props.preferredEvents.map(event => event._id))
-    console.log(props.thisUser.preferredEvents)
+    const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
 
     const handleOnDragEnd = (result) => {
+        // result is array of EventObjs in the order of the drag & drop.
         if (!result.destination) return;
         const items = Array.from(preferredEvents);
         const [reorderedItem] = items.splice(result.source.index, 1);
@@ -33,8 +38,21 @@ const dashboard = (props: {thisUser: DatedObj<UserObj>, preferredEvents: DatedOb
         })
         .catch(e => {console.log(e)});
     }
+
+    const onSubmit = () => {
+        axios.post("/api/submission", {
+            user: props.thisUser._id,
+            top3events: [preferredEvents[0], preferredEvents[1], preferredEvents[2]], // more elegant way to do this
+        }).then(res => {
+            console.log("you have submitted your top 3 events!", res.data)
+        })
+        .catch(e => {console.log(e)})
+        .finally(() => setIsSubmitting(false));
+    }
+
     return (
         <Container>
+            <p>Here are all your matches ;)<br/>Please drag them into order of preference and hit submit to submit your top 3 event selections.</p>
             <DragDropContext onDragEnd={handleOnDragEnd}>
                 <Droppable droppableId="tasks-master">
                     {(provided) => (
@@ -59,7 +77,14 @@ const dashboard = (props: {thisUser: DatedObj<UserObj>, preferredEvents: DatedOb
                     )}
                 </Droppable>
             </DragDropContext>
-
+            <HandwrittenButton onClick={() => setIsSubmitting(true)} disabled={!preferredEvents}>Submit!</HandwrittenButton>
+            <Modal isOpen={isSubmitting} setIsOpen={setIsSubmitting}>
+                <p>Are you sure you want to submit your events? Please make sure you are 100% sure about your top 3 choices, as this action cannot be undone.</p>
+                <div className="flex gap-2">
+                    <HandwrittenButton onClick={onSubmit}>Submit</HandwrittenButton>
+                    <Button onClick={() => setIsSubmitting(false)}>Cancel</Button>
+                </div>
+            </Modal>
         </Container>
     )
 }
