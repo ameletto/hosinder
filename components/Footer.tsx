e2648e742b1bd50030f2faf01e47db16d1248996
Laura Gao <lauragao86868@gmail.com>
2021-09-22 17:26:52 -0400

    Creating footer

---
+++ components/Footer.tsx
@@ -0,0 +1,20 @@
+export default function Footer() {
+  return (
+    <div className="max-w-7xl mx-auto h-16 flex items-center px-4 mt-12">
+      <span className="dark:text-gray-300 opacity-50">
+        Made with ♥ by{" "}
+        <a
+          className="underline"
+          href="https://www.youtube.com/channel/UCstSEHcCLMGdac9wkbMeAIw"
+        >
+          Laura Gao
+        </a>{" "}
+        and{" "}
+        <a className="underline" href="https://twitter.com/amyli0">
+          Amy Li
+        </a>{" "}
+        from TOPS Class of 2023
+      </span>
+    </div>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,23 +5,23 @@ import ReactModal from "react-modal";
 import Navbar from "../components/Navbar";
 import "../styles/globals.css";
 import "../styles/nprogress.css";
+import Footer from "../components/Footer";
 
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
 
-export default function App({Component, pageProps}) {
-    const router = useRouter();
-    return (
-        <Provider session={pageProps.session}>
-            {!["/"].includes(router.route) && (
-                <Navbar/>
-            )}
-            <div id="app-root">
-                <Component {...pageProps} />
-            </div>
-        </Provider>
-    );
+export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  return (
+    <Provider session={pageProps.session}>
+      {!["/"].includes(router.route) && <Navbar />}
+      <div id="app-root">
+        <Component {...pageProps} />
+      </div>
+      <Footer />
+    </Provider>
+  );
 }
 
 ReactModal.setAppElement("#app-root");
